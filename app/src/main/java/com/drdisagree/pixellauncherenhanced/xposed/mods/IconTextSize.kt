88c78f185ce815f539f71d777366061a37102e2b
lgernier <108574792+lgernierO@users.noreply.github.com>
2026-07-19 00:18:30 +0800

    Fix icon and text sizing on latest Pixel Launcher Canary

--- app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/IconTextSize.kt
+++ app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/IconTextSize.kt
@@ -6,18 +6,24 @@ import com.drdisagree.pixellauncherenhanced.data.common.Constants.LAUNCHER_TEXT_
 import com.drdisagree.pixellauncherenhanced.xposed.ModPack
 import com.drdisagree.pixellauncherenhanced.xposed.mods.LauncherUtils.Companion.reloadLauncher
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.XposedHook.Companion.findClass
-import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.getField
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.getFieldSilently
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookConstructor
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookMethod
-import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.setField
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.setFieldSilently
 import com.drdisagree.pixellauncherenhanced.xposed.utils.XPrefs.Xprefs
 
 class IconTextSize(context: Context) : ModPack(context) {
 
+    private class ProfileBuildState(
+        val parent: ProfileBuildState?
+    ) {
+        var workspaceProfileCreated = false
+        var allAppsProfileCreated = false
+    }
+
     private var iconSizeModifier = 1f
     private var textSizeModifier = 1f
+    private val profileBuildState = ThreadLocal<ProfileBuildState>()
 
     override fun updatePrefs(vararg key: String) {
         Xprefs.apply {
@@ -35,150 +41,152 @@ class IconTextSize(context: Context) : ModPack(context) {
 
     override fun handleLoadPackage(packageName: String, classLoader: ClassLoader) {
         val deviceProfileClass = findClass("com.android.launcher3.DeviceProfile")
-        val deviceProfileBuilderClass = findClass(
+        val legacyDeviceProfileBuilderClass = findClass(
             $$"com.android.launcher3.DeviceProfile$Builder",
             suppressError = true
         )
+        val deviceProfileBuilderClass = findClass(
+            "com.android.launcher3.deviceprofile.DeviceProfileBuilder",
+            suppressError = true
+        )
+        val workspaceProfileClass = findClass(
+            "com.android.launcher3.deviceprofile.WorkspaceProfile",
+            suppressError = true
+        )
+        val allAppsProfileClass = findClass(
+            "com.android.launcher3.deviceprofile.AllAppsProfile",
+            suppressError = true
+        )
 
-        fun Any.hookDeviceProfile() {
-            val temp = getFieldSilently("iconSizePx") as? Int
-
-            if (temp != null) {
-                var iconSizePx = getField("iconSizePx") as Int
-                var folderIconSizePx = getField("folderIconSizePx") as Int
-                var folderChildIconSizePx = getField("folderChildIconSizePx") as Int
-                var allAppsIconSizePx = getFieldSilently("allAppsIconSizePx") as? Int
-                var iconTextSizePx = getField("iconTextSizePx") as Int
-                var folderLabelTextSizePx = getField("folderLabelTextSizePx") as Int
-                var folderChildTextSizePx = getField("folderChildTextSizePx") as Int
-                var allAppsIconTextSizePx =
-                    getFieldSilently("allAppsIconTextSizePx") as? Float
-                var folderCellWidthPx = getField("folderCellWidthPx") as Int
-                var folderCellHeightPx = getField("folderCellHeightPx") as Int
-
-                iconSizePx = (iconSizePx * iconSizeModifier).toInt()
-                folderIconSizePx = (folderIconSizePx * iconSizeModifier).toInt()
-                folderChildIconSizePx = (folderChildIconSizePx * iconSizeModifier).toInt()
-                if (allAppsIconSizePx != null) {
-                    allAppsIconSizePx = (allAppsIconSizePx * iconSizeModifier).toInt()
-                }
-                iconTextSizePx = (iconTextSizePx * textSizeModifier).toInt()
-                folderLabelTextSizePx = (folderLabelTextSizePx * textSizeModifier).toInt()
-                folderChildTextSizePx = (folderChildTextSizePx * textSizeModifier).toInt()
-                if (allAppsIconTextSizePx != null) {
-                    allAppsIconTextSizePx *= textSizeModifier
-                }
-                folderCellWidthPx = (folderCellWidthPx * iconSizeModifier).toInt()
-                folderCellHeightPx = (folderCellHeightPx * iconSizeModifier).toInt()
-
-                setField("iconSizePx", iconSizePx)
-                setField("folderIconSizePx", folderIconSizePx)
-                setField("folderChildIconSizePx", folderChildIconSizePx)
-                if (allAppsIconSizePx != null) {
-                    setField("allAppsIconSizePx", allAppsIconSizePx)
-                }
-                setField("iconTextSizePx", iconTextSizePx)
-                setField("folderLabelTextSizePx", folderLabelTextSizePx)
-                setField("folderChildTextSizePx", folderChildTextSizePx)
-                if (allAppsIconTextSizePx != null) {
-                    setField("allAppsIconTextSizePx", allAppsIconTextSizePx)
-                }
-                setField("folderCellWidthPx", folderCellWidthPx)
-                setField("folderCellHeightPx", folderCellHeightPx)
-            } else {
-                val mWorkspaceProfile = getFieldSilently("workspaceProfile")
-                if (mWorkspaceProfile == null) {
-                    // New launcher version - field not available, skip this section
-                    return
-                }
-                var mWorkspaceProfileIconSizePx =
-                    mWorkspaceProfile.getField("iconSizePx") as Int
-                var mWorkspaceProfileIconTextSizePx =
-                    mWorkspaceProfile.getField("iconTextSizePx") as Int
-
-                val mFolderProfile = getFieldSilently("folderProfile")
-                var mFolderProfileFolderIconSizePx =
-                    mFolderProfile?.getFieldSilently("folderIconSizePx") as? Int
-                var mFolderProfileLabelTextSizePx =
-                    mFolderProfile?.getField("labelTextSizePx") as Int
-                var mFolderProfileFolderChildIconSizePx =
-                    mFolderProfile?.getField("childIconSizePx") as Int
-                var mFolderProfileFolderChildTextSizePx =
-                    mFolderProfile?.getField("childTextSizePx") as Int
-                var mFolderProfileFolderCellWidthPx =
-                    mFolderProfile?.getField("cellWidthPx") as Int
-                var mFolderProfileFolderCellHeightPx =
-                    mFolderProfile?.getField("cellHeightPx") as Int
-
-                val mAllAppsProfile = getFieldSilently("allAppsProfile")
-                var mAllAppsProfileAllAppsIconSizePx =
-                    mAllAppsProfile?.getField("iconSizePx") as Int
-                var mAllAppsProfileAllAppsIconTextSizePx =
-                    mAllAppsProfile?.getField("iconTextSizePx") as Float
-
-                var folderIconSizePx = getFieldSilently("folderIconSizePx") as? Int
-                var folderLabelTextSizePx = getFieldSilently("folderLabelTextSizePx") as? Int
-
-                mWorkspaceProfileIconSizePx =
-                    (mWorkspaceProfileIconSizePx * iconSizeModifier).toInt()
-                mWorkspaceProfileIconTextSizePx =
-                    (mWorkspaceProfileIconTextSizePx * textSizeModifier).toInt()
-                if (mFolderProfileFolderIconSizePx != null) {
-                    mFolderProfileFolderIconSizePx =
-                        (mFolderProfileFolderIconSizePx * iconSizeModifier).toInt()
-                }
-                mFolderProfileLabelTextSizePx =
-                    (mFolderProfileLabelTextSizePx * textSizeModifier).toInt()
-                mFolderProfileFolderChildIconSizePx =
-                    (mFolderProfileFolderChildIconSizePx * iconSizeModifier).toInt()
-                mFolderProfileFolderChildTextSizePx =
-                    (mFolderProfileFolderChildTextSizePx * textSizeModifier).toInt()
-                mFolderProfileFolderCellWidthPx =
-                    (mFolderProfileFolderCellWidthPx * iconSizeModifier).toInt()
-                mFolderProfileFolderCellHeightPx =
-                    (mFolderProfileFolderCellHeightPx * iconSizeModifier).toInt()
-                mAllAppsProfileAllAppsIconSizePx =
-                    (mAllAppsProfileAllAppsIconSizePx * iconSizeModifier).toInt()
-                mAllAppsProfileAllAppsIconTextSizePx *= textSizeModifier
-                if (folderIconSizePx != null) {
-                    folderIconSizePx = (folderIconSizePx * iconSizeModifier).toInt()
-                }
-                if (folderLabelTextSizePx != null) {
-                    folderLabelTextSizePx =
-                        (folderLabelTextSizePx * textSizeModifier).toInt()
-                }
-
-                mWorkspaceProfile.setField("iconSizePx", mWorkspaceProfileIconSizePx)
-                mWorkspaceProfile.setField("iconTextSizePx", mWorkspaceProfileIconTextSizePx)
-                mFolderProfile.setFieldSilently("folderIconSizePx", mFolderProfileFolderIconSizePx)
-                mFolderProfile.setField("labelTextSizePx", mFolderProfileLabelTextSizePx)
-                mFolderProfile.setField("childIconSizePx", mFolderProfileFolderChildIconSizePx)
-                mFolderProfile.setField("childTextSizePx", mFolderProfileFolderChildTextSizePx)
-                mFolderProfile.setField("cellWidthPx", mFolderProfileFolderCellWidthPx)
-                mFolderProfile.setField("cellHeightPx", mFolderProfileFolderCellHeightPx)
-                mAllAppsProfile.setField("iconSizePx", mAllAppsProfileAllAppsIconSizePx)
-                mAllAppsProfile.setField("iconTextSizePx", mAllAppsProfileAllAppsIconTextSizePx)
-                setFieldSilently("folderIconSizePx", folderIconSizePx)
-                setFieldSilently("folderLabelTextSizePx", folderLabelTextSizePx)
-            }
-        }
-
+        // Older Launcher3 versions keep all dimensions directly in DeviceProfile.
         deviceProfileClass
             .hookConstructor()
             .runAfter { param ->
-                param.thisObject.apply {
-                    hookDeviceProfile()
-                }
+                param.thisObject?.scaleLegacyDeviceProfile()
             }
 
-        deviceProfileBuilderClass
-            .hookMethod("build")
-            .suppressError()
-            .runAfter { param ->
-                try {
-                    param.result?.hookDeviceProfile()
-                } catch (_: Throwable) {
+        val builderClasses = listOfNotNull(
+            legacyDeviceProfileBuilderClass,
+            deviceProfileBuilderClass
+        ).distinct()
+
+        // Newer launchers build immutable WorkspaceProfile and AllAppsProfile objects. Their
+        // final fields can no longer be reliably changed after DeviceProfile is constructed, so
+        // keep track of the build call and alter the profile constructor arguments instead.
+        builderClasses.forEach { builderClass ->
+            builderClass
+                .hookMethod("build")
+                .suppressError()
+                .runBeforeAndAfter(
+                    beforeCallback = {
+                        profileBuildState.set(ProfileBuildState(profileBuildState.get()))
+                    },
+                    afterCallback = {
+                        val parent = profileBuildState.get()?.parent
+                        if (parent == null) {
+                            profileBuildState.remove()
+                        } else {
+                            profileBuildState.set(parent)
+                        }
+                    }
+                )
+
+            builderClass
+                .hookMethod("createFolderProfile")
+                .suppressError()
+                .runAfter { param ->
+                    val state = profileBuildState.get() ?: return@runAfter
+                    param.result?.scaleFolderProfile(
+                        folderIconAlreadyScaled = state.workspaceProfileCreated
+                    )
                 }
+        }
+
+        workspaceProfileClass
+            .hookConstructor()
+            .suppressError()
+            .runBefore { param ->
+                val state = profileBuildState.get() ?: return@runBefore
+                if (state.workspaceProfileCreated) return@runBefore
+
+                // Pixel Launcher Canary: WorkspaceProfile(float iconScale, int iconSizePx,
+                // int iconTextSizePx, ...).
+                val iconSizePx = param.args.getOrNull(1) as? Int ?: return@runBefore
+                val iconTextSizePx = param.args.getOrNull(2) as? Int ?: return@runBefore
+
+                param.args[1] = (iconSizePx * iconSizeModifier).toInt()
+                param.args[2] = (iconTextSizePx * textSizeModifier).toInt()
+                state.workspaceProfileCreated = true
             }
+
+        allAppsProfileClass
+            .hookConstructor()
+            .suppressError()
+            .runBefore { param ->
+                val state = profileBuildState.get() ?: return@runBefore
+                if (state.allAppsProfileCreated) return@runBefore
+
+                // Pixel Launcher Canary: AllAppsProfile(Point, int cellHeightPx,
+                // int iconSizePx, float iconTextSizePx, ...).
+                val iconSizePx = param.args.getOrNull(2) as? Int ?: return@runBefore
+                val iconTextSizePx = param.args.getOrNull(3) as? Float ?: return@runBefore
+
+                param.args[2] = (iconSizePx * iconSizeModifier).toInt()
+                param.args[3] = iconTextSizePx * textSizeModifier
+                state.allAppsProfileCreated = true
+            }
+    }
+
+    private fun Any.scaleLegacyDeviceProfile() {
+        if (getFieldSilently("iconSizePx") !is Int) return
+
+        listOf(
+            "iconSizePx",
+            "folderIconSizePx",
+            "folderChildIconSizePx",
+            "allAppsIconSizePx",
+            "folderCellWidthPx",
+            "folderCellHeightPx"
+        ).forEach { fieldName ->
+            scaleNumericField(fieldName, iconSizeModifier)
+        }
+
+        listOf(
+            "iconTextSizePx",
+            "folderLabelTextSizePx",
+            "folderChildTextSizePx",
+            "allAppsIconTextSizePx"
+        ).forEach { fieldName ->
+            scaleNumericField(fieldName, textSizeModifier)
+        }
+    }
+
+    private fun Any.scaleFolderProfile(folderIconAlreadyScaled: Boolean) {
+        listOf(
+            "childIconSizePx",
+            "cellWidthPx",
+            "cellHeightPx"
+        ).forEach { fieldName ->
+            scaleNumericField(fieldName, iconSizeModifier)
+        }
+
+        if (!folderIconAlreadyScaled) {
+            scaleNumericField("folderIconSizePx", iconSizeModifier)
+        }
+
+        listOf(
+            "labelTextSizePx",
+            "childTextSizePx"
+        ).forEach { fieldName ->
+            scaleNumericField(fieldName, textSizeModifier)
+        }
+    }
+
+    private fun Any.scaleNumericField(fieldName: String, modifier: Float) {
+        when (val value = getFieldSilently(fieldName)) {
+            is Int -> setFieldSilently(fieldName, (value * modifier).toInt())
+            is Float -> setFieldSilently(fieldName, value * modifier)
+            is Double -> setFieldSilently(fieldName, value * modifier)
+        }
     }
 }
